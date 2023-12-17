cd1c40da63edbf814c7b6c94847921d561af4b53
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2023-12-17 20:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function fibonacci(num) {
-// your code here
-}
+function byteSize(str) {
+  // Check for empty string
+  if (str === '') {
+    return 0;
+  }
 
-module.exports = fibonacci;
+  // Create a TextEncoder instance with UTF-8 encoding
+  const encoder = new TextEncoder('utf-8');
+
+  // Encode the string and return the byte length
+  const encoded = encoder.encode(str);
+  return encoded.length;
+}
